742edb00e228d4ee249310c3d95004ba5151bb01
Symul Kabir <saimonpranta@gmail.com>
2026-08-25 17:20:56 +0600

    added dashboard and resume page

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerForgeAI.Controllers;
+
+[Route("dashboard")]
+public class DashboardController : Controller
+{
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet("profile")]
+    public IActionResult Profile()
+    {
+        return View();
+    }
+    [HttpGet("resume")]
+    public IActionResult Resume()
+    {
+        return View();
+    }
+}
